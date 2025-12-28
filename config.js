08b27bdf0f9f6ad9f71280e8d490c1586eb71874
bobby175 <42850033+bobby175@users.noreply.github.com>
2025-12-28 08:40:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Semua link media dipusatkan di sini
+window.APP_CONFIG = {
+  audio: "https://bit.ly/4pyMi6i",
+  photo: "https://github.com/bobby175/my-porto/raw/main/475526283_639457441974249_5752591344848529673_n.jpg",
+  gift: "https://github.com/bobby175/my-porto/raw/main/mlbb-jjk-jjk-mlbb.gif",
+  preview: "https://github.com/bobby175/my-porto/raw/main/475526283_639457441974249_5752591344848529673_n.jpg",
+  instagram: "https://www.instagram.com/wulan_chaaan_?igsh=MWVkMXlvbm9pNnA0cw=="
+};
